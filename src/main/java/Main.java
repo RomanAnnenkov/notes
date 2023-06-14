5339b7008a49871f2dadecd2ffb917828386518d
Roman Annenkov <roman.annenkov@gmail.com>
2023-06-14 16:14:48 +0300

    added view with some plugs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+import controllers.NoteController;
+import model.converter.ConverterNote;
+import model.io.InputOutputOperation;
+import model.io.InputOutputOperationFile;
+import model.storage.Storage;
+import model.storage.StorageFile;
+import views.ViewNote;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("notes");
+        InputOutputOperation file = new InputOutputOperationFile("notes.txt");
+        Storage storage = new StorageFile(file, new ConverterNote());
+        ViewNote viewNote = new ViewNote(new NoteController(storage));
+        viewNote.run();
     }
 }
---
+++ src/main/java/views/ViewNote.java
@@ -0,0 +1,77 @@
+package views;
+
+import controllers.NoteController;
+import model.Note;
+
+import java.util.Scanner;
+
+public class ViewNote {
+    private final NoteController noteController;
+    private final Scanner scanner = new Scanner(System.in);
+
+    public ViewNote(NoteController noteController) {
+        this.noteController = noteController;
+    }
+
+    public void run() {
+        String help = """
+                Команды:
+                create - создать заметку
+                read - показать заметку
+                all - показать все заметки
+                edit - изменить записку
+                remove - удалить записку
+                exit - выход""";
+        while (true) {
+            System.out.print("Ввод: ");
+            String userCommand = scanner.nextLine().toLowerCase();
+            if (userCommand.equals("exit")) {
+                return;
+            }
+            switch (userCommand) {
+                case "create":
+                    createNote();
+                    break;
+                case "read":
+                    readNote();
+                    break;
+                case "all":
+                    showAllNotes();
+                    break;
+                case "edit":
+                    updateNote();
+                    break;
+                case "remove":
+                    removeNote();
+                    break;
+                default:
+                    System.out.println(help);
+            }
+        }
+    }
+
+    private void createNote() {
+        int id = noteController.readAllNotes().size() + 1;//TODO add normal generate id
+        System.out.print("Заголовок: ");
+        String header = scanner.nextLine();
+        System.out.print("Текст: ");
+        String text = scanner.nextLine();
+        noteController.saveNote(new Note(id, header, text));
+    }
+
+    private void readNote() {
+        System.out.println("read note");
+    }
+
+    private void showAllNotes() {
+        System.out.println("read all notes");
+    }
+
+    private void updateNote() {
+        System.out.println("update note");
+    }
+
+    private void removeNote() {
+        System.out.println("remove note");
+    }
+}
