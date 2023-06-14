1e15ea017d82cb6a881ceac99a70c5c7679a00a4
Roman Annenkov <roman.annenkov@gmail.com>
2023-06-14 15:07:30 +0300

    added NoteController class

---
+++ src/main/java/controllers/NoteController.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import model.AbstractNote;
+import model.storage.Storage;
+import java.util.List;
+
+public class NoteController {
+    private Storage storage;
+
+    public NoteController(Storage storage) {
+        this.storage = storage;
+    }
+
+    public void saveNote(AbstractNote note) {
+        storage.createNote(note);
+    }
+
+    public AbstractNote readNote(int id) {
+        return storage.getNote(id);
+    }
+
+    public List<AbstractNote> readAllNotes() {
+        return storage.getAllNotes();
+    }
+
+    public AbstractNote updateNote(AbstractNote note) {
+        return storage.updateNote(note);
+    }
+
+    public AbstractNote deleteNote(AbstractNote note) {
+        return storage.removeNote(note);
+    }
+}
