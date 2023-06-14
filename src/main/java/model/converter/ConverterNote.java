5838ee0f56f7360c0ed6ee2299aee1b06cb131b5
Roman Annenkov <roman.annenkov@gmail.com>
2023-06-14 22:59:20 +0300

    corrected naming

--- src/main/java/model/converter/ConverterNote.java
+++ src/main/java/model/converter/ConverterNote.java
@@ -3,26 +3,26 @@ package model.converter;
 import model.Note;
 
 public class ConverterNote implements Converter {
-    String spliterator;
+    String separator;
 
     public ConverterNote() {
-        this.spliterator = ",";
+        this.separator = ",";
     }
 
-    public ConverterNote(String spliterator) {
-        this.spliterator = spliterator;
+    public ConverterNote(String separator) {
+        this.separator = separator;
     }
 
     @Override
     public String convert(Note note) {
-        return note.getId() + spliterator +
-                note.getHeader() + spliterator +
+        return note.getId() + separator +
+                note.getHeader() + separator +
                 note.getText();
     }
 
     @Override
     public Note convert(String str) {
-        String[] elements = str.split(spliterator);
+        String[] elements = str.split(separator);
         return new Note(Integer.parseInt(elements[0]), elements[1], elements[2]);
     }
 }
